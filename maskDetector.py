e8309c9421203178a13895e244f79580bc98a233
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-01-05 13:09:31 -0500

    updated

--- maskDetector.py
+++ maskDetector.py
@@ -29,10 +29,7 @@ RTC_CONFIGURATION = RTCConfiguration(
 noseCascade = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
 mouthCascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 
-st.write("Press start to turn on Camera!")
-st.write("If camera doesn't turn on, click the select device button, change the camera input and reload your screen!")
-
-
+st.write("Press start to turn on camera and see if you are wearing your mask or not!")
 
 def maskDetector():
     class OpenCVVideoProcessor(VideoProcessorBase):
@@ -41,8 +38,9 @@ def maskDetector():
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             nose = noseCascade.detectMultiScale(gray, 1.3, 5)
             mouth = mouthCascade.detectMultiScale(gray, 1.3, 5)
+            cv2.rectangle(img, pt1=(0, 0), pt2=(700, 80), color=(0, 0, 0), thickness=-1)
             if len(nose) != 0 and len(mouth) != 0:
-                cv2.putText(img, "Not Wearing Mask!", (20, 50), cv2.FONT_HERSHEY_DUPLEX,1, 255)
+                cv2.putText(img, "Not Wearing Mask", (20, 50), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
             else:
                 cv2.putText(img, "Good, Wearing Mask!", (20, 50), cv2.FONT_HERSHEY_DUPLEX,1, 255)
             return av.VideoFrame.from_ndarray(img, format="bgr24")
@@ -61,6 +59,13 @@ def maskDetector():
             "autoPlay": True,
         },
     )
+
+    # Info Block
+    st.write("If camera doesn't turn on, please ensure that your camera permissions are on!")
+    with st.expander("Steps to enable permission"):
+        st.write("1. Click the lock button at the top left of the page")
+        st.write("2. Slide the camera slider to on")
+        st.write("3. Reload your page!")
 
 if __name__ == "__main__":
     maskDetector()
